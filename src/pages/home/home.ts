f379aeb20da61f7e2cbd5bdf5d0eeadc4d5bef5c
changyahui <hui@MacBook-Air.local>
2017-11-28 18:02:00 +0800

    mousegame

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,82 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  image: any;
+  ranK: any;
+  count: number = 0;
+  time: number = 60;
+  timer: any;
+  img: Array<any> = new Array(9);
+  clamp: any;
+  type: Array<any> = ['mouse', 'mouse', 'mouse', 'mouse', 'mouse', 'mouse', 'clamp', 'clamp', 'clamp']
   constructor(public navCtrl: NavController) {
+    for (let i = 0; i < 9; i++) {
+      // this.img[i] = { pic: 'assets/img/gameframe.png', type: '' };
+      this.img[i]='assets/img/gameframe.png';
+    }
+    console.log(this.img)
+    //  this.img.forEach(element => {
+    //   element='assets/img/gameframe.png';
+    // });
 
   }
 
+  start() {
+    this.time = 60;
+    this.count = 0;
+    this.image = document.getElementsByTagName('img');
+    console.log(this.image)
+    this.show();
+    this.showtime();
+    // setTimeout(() => {
+    //   this.track();
+    // }, 1000)
+  }
+  showtime() {
+    this.time -= 1;
+    if (this.time > 0) {
+      setTimeout(() => {
+        this.showtime();
+      }, 1000)
+    }
+  }
+  show() {
+    if (this.time > 0) {
+      this.ranK = Math.floor(Math.random() * 9);
+      let typeindex = Math.floor(Math.random() * 9);
+      console.log(this.ranK)
+      if (this.type[typeindex] == 'mouse') {
+        this.img[this.ranK] = 'assets/img/show.png';
+        // this.img[this.ranK].type = 'mouse';
+      } else {
+        this.img[this.ranK] = 'assets/img/clamp.png';
+        // this.img[this.ranK].type = 'clamp';
+      }
+      this.timer = setTimeout(() => {
+        this.hidden();
+      }, 1000);
+    }
+  }
+
+  hidden() {
+    this.img[this.ranK] = 'assets/img/gameframe.png';
+    // this.img[this.ranK].type = '';
+    this.show();
+  }
+
+  punch(num) {
+    // console.log(type)
+    if (num == this.ranK) {
+      if (this.img[this.ranK] == 'assets/img/show.png') {
+        this.count++;
+        this.img[this.ranK] = 'assets/img/gameframe.png';
+        // this.img[this.ranK].type = '';
+      } else {
+        this.count--;
+        this.img[this.ranK] = 'assets/img/gameframe.png';
+        // this.img[this.ranK].type = '';
+      }
+      this.ranK = null;
+    }
+  }
 }
